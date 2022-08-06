dd7c2fd0552b3571c4f9ecd26c9a00e65a7ceaff
Rocco <grigoreandrey23@gmail.com>
2022-08-06 13:55:02 +0300

    add hitable abstract class

---
+++ include/Hitable.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Ray.hpp"
+
+struct HitRecord
+{
+    f32 t;
+    Vec3 p;
+    Vec3 normal;
+};
+
+class Hitable
+{
+public:
+    virtual bool hit(const Ray& r, f32 tMin, f32 tMax, HitRecord& rec) = 0;
+};
+
